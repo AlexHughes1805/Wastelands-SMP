6d5909a56caa98eb0cd2ca17d451cc2dcaff69ad
Alex H <alexhughes1805@gmail.com>
2026-07-03 16:09:46 +0100

    Initial Commit

---
+++ src/client/java/oc/wastelands/client/WastelandsSMPClient.java
@@ -0,0 +1,10 @@
+package oc.wastelands.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WastelandsSMPClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
